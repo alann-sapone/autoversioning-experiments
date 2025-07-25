3a6d40c0122db87810b83d211adc988f658666a7
Alann Sapone <sapone.alann@gmail.com>
2025-07-25 19:58:01 +0200

    chore: Improves commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-case": [2, "always", ["sentence-case", "start-case"]],
+    "body-max-line-length": [0, "never"]
   },
 };
